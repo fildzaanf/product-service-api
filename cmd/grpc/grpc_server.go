e79ec2d5ad5f68864fd8458d1467c15e207a9aa8
fildzaanf <hanisahfildza@gmail.com>
2025-11-07 11:25:41 +0700

    add grpc server main

---
+++ cmd/grpc/grpc_server.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"product-service-api/infrastructure/config"
+	"product-service-api/infrastructure/database"
+	"product-service-api/pkg/middleware"
+
+	"github.com/common-nighthawk/go-figure"
+
+	productGRPC "product-service-api/internal/product/adapter/handler/grpc"
+
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	middleware.InitLogger()
+
+	godotenv.Load()
+	config, err := config.LoadConfig()
+	if err != nil {
+		logrus.Fatalf("[ERROR] failed to load configuration: %v", err)
+	}
+
+	psql := database.ConnectPostgreSQL(false)
+
+	userServiceAddress := config.USERSERVICE.USER_HOST + ":" + config.USERSERVICE.USER_PORT
+	userConn, err := grpc.NewClient(userServiceAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("[ERROR] failed to connect to user service: %v", err)
+	}
+	defer userConn.Close()
+
+	server := grpc.NewServer(
+		grpc.UnaryInterceptor(middleware.JWTUnaryInterceptor()),
+	)
+
+	productGRPC.RegisterProductServices(server, psql, userConn)
+
+	reflection.Register(server)
+
+	address := config.PRODUCTSERVICE.PRODUCT_HOST + ":" + config.PRODUCTSERVICE.PRODUCT_PORT
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("[FATAL] failed to listen product service: %v", err)
+	}
+
+	fig := figure.NewFigure("PRODUCT SERVICE API", "small", true)
+	fig.Print()
+
+	fmt.Printf("\n📡 Listening on %s\n", address)
+
+	if err := server.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
